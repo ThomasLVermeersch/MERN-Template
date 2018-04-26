79c5f30aecb78e1faba708a56b1c1ae36eeec188
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2018-04-26 15:04:58 -0700

    user schema

---
+++ schemas/user.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true
+	},
+	first_name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	last_name: {
+		type: String,
+		required: true,
+		trim: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	passwordConf:{
+		type: String,
+		required: true
+	}
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
