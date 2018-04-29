9137d0dc45e65fa084aebda11066a11d3001af66
Thomas Vermeersch <thomaslvermeersch@gmail.com>
2018-04-29 01:13:38 -0700

    Added login, password hashing and authentication

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PythonShell = require('python-shell');
 const mongoose = require('mongoose');
 const config = require('./config');
 const User = require('./schemas/user');
-
+const session = require('express-session');
 mongoose.connect(config.MONGODB_URL);
 
 var db = mongoose.connection;
@@ -30,34 +30,71 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+//use sessions for tracking logins
+app.use(session({
+  secret: 'secret key',
+  resave: true,
+  saveUninitialized: false
+}));
+
 
 //routes
 app.get('/', (req, res) =>{
 	res.render('index');
 });
 
+app.post('/signin', function(req, res, next) {
+  if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+       if (error || !user) {
+         var err = new Error('Wrong email or password.');
+         err.status = 401;
+         return next(err);
+      } else {
+         req.session.userId = user._id;
+         console.log(req.session.userId);
+         return res.redirect('/');
+       }
+     });
+   } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    var err = new Error('All fields required.');
+    err.status = 400;
+     return next(err);
+   }
+});
+
 app.post('/signup', (req, res) =>{
-  if (true) {
+  if (req.body.email &&
+     req.body.fname &&
+     req.body.lname &&
+     req.body.password &&
+     req.body.passwordConf) {
     var userData = {
       email: req.body.email,
       first_name: req.body.fname,
       last_name: req.body.lname,
-      password: req.body.password,
-      passwordConf: req.body.passwordConf
+      password: req.body.password
     }
     //use schema.create to insert data into the db
-    User.create(userData, function (err, User) {
+    User.create(userData, function (err, user) {
       console.log(userData);
       if (err) {
         console.log(err);
         return err;
       } else {
+        req.session.userId = user._id;
         return res.redirect('/');
       }
     });
+  } else {
+    console.log(req.body);
   }
 });
 
 app.listen('3000', () =>{
 	console.log('Server started on port 3000');
 });
+
+
